5b4d326bfdd061d35200ad4bdeb7abde452e2624
wangsen <wangs@tongxinyiliao.com>
2015-09-07 23:47:26 +0800

    .

--- packages/sen-camera-album/package.js
+++ packages/sen-camera-album/package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'wuyuedefeng:sen-camera-album',
-    version: '0.1.8',
+    version: '0.1.9',
     // Brief, one-line summary of the package.
     summary: 'camera album get images base cordova',
     // URL to the Git repository containing the source code for this package.
